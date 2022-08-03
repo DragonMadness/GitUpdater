a468dd34116f2aa52c0f1505f06c12bfc5cb7ad7
DragonMadness_ <supervasya45@gmail.com>
2022-08-04 01:05:08 +0300

    Added some messages to input()

--- Main.py
+++ Main.py
@@ -7,9 +7,9 @@ with open("./config.json", mode="r") as file:
 
 username = data["username"]
 github_token = data["github_token"]
-target_dir = input()
-repo = input()
-branch = input()
+target_dir = input("Target directory to clone to: ")
+repo = input("Repo owner and its name")
+branch = input("Branch of the repo to clone")
 remote = f"https://{username}:{github_token}@github.com/{repo}.git"
 
 try:
