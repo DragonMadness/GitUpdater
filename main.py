dff9350a60536d845e4c1601124b33e75cb3c504
Muxel <41229495+sllavon@users.noreply.github.com>
2022-08-08 22:48:15 +0300

    Update and rename Main.py to main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from json import loads
+from os import mkdir
+from shutil import rmtree
+
+from git import Repo
+
+from config import GITHUB_TOKEN, USERNAME
+
+with open("./config.json", mode="r") as file:
+    data = loads(file.read())
+
+target_dir: str = input('Target directory to clone to: ')
+repo: str = input('Repo owner and its name: ')
+branch: str = input('Branch of the repo to clone: ')
+
+remote: str = f'https://{USERNAME}:{GITHUB_TOKEN}@github.com/{repo}.git'
+
+try:
+    rmtree(target_dir)
+    print(f'Directory {target_dir} was removed successfully')
+    
+except FileNotFoundError:
+    mkdir(target_dir)
+
+print('Started cloning the repo...')
+Repo.clone_from(remote, target_dir, branch=branch)
+print('Cloned successfully')
